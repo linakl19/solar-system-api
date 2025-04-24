1d6bdf4e5946cb9afd76fbf7cdbb574072a1b926
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-24 13:43:06 -0700

    endpoints to create and get all plantes

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,44 +1,44 @@
-from flask import Blueprint, abort, make_response
+from flask import Blueprint, request
+from app.models.planets import Planet
+from ..db import db
 # from app.models.planets import list_of_planets
 
 # Create blueprint
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-# Create endpoints
-# @planets_bp.get("")
-# def get_all_planets():
-#     planets_response = []
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    diameter = request_body["diameter"]
     
-#     for planet in list_of_planets:
-#         planets_response.append(dict(
-#             id = planet.id,
-#             name = planet.name,
-#             description = planet.description,
-#             diameter = planet.diameter 
-#         ))
+    new_planet = Planet(name=name, description=description, diameter=diameter)
+    db.session.add(new_planet)
+    db.session.commit()
     
-#     return planets_response
-
-# # wave 2
-# @planets_bp.get("/<planet_id>")
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
-#     return dict(
-#             id = planet.id,
-#             name = planet.name,
-#             description = planet.description,
-#             diameter = planet.diameter)
-
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except ValueError:
-#         invalid = {"message": f"Planet id {planet_id} is invalid!"}
-#         abort(make_response(invalid, 400))
-        
-#     for planet in list_of_planets:
-#         if planet.id == planet_id:
-#             return planet
-        
-#     not_found = {"message": f"Planet {planet_id} was not found"}
-#     abort(make_response(not_found, 404))
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "diameter": new_planet.diameter 
+    }
+    return response, 201
+    
+@planets_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    
+    planets_response = []
+    
+    for planet in planets:
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "diameter": planet.diameter
+            }
+        )
+    return planets_response
